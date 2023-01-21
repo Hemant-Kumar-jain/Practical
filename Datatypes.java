cc2f5e457af9b56c86b2145b70eb364e5da1b5e1
Hemant-Kumar-jain <122785139+Hemant-Kumar-jain@users.noreply.github.com>
2023-01-21 00:32:25 -0800

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,20 @@
+class Datatypes{
+public static void main(String[] args){
+int a=2;
+float b=4;
+double c=8;
+long l=16;
+byte d=32;
+
+double e=c/b;
+System.out.println(e);
+double f=b/a;
+System.out.println(f);
+double g=c/a;
+System.out.println(g);
+double h=g+l;
+System.out.println(h);
+int i=a/d;
+System.out.println(i);
+}
+}
